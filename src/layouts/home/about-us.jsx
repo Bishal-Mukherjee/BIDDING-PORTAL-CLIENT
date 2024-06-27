e4797d1294d2aa86ab5ed8b6b8d2f56a787f37a8
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2024-06-27 22:09:40 +0530

    feat: animations added to homepage

--- src/layouts/home/about-us.jsx
+++ src/layouts/home/about-us.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prop-types */
-import React from 'react';
-// import { useNavigate } from 'react-router-dom';
+
+import React, { useRef, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import { Box, Grid, Stack, Divider, Container, Typography } from '@mui/material';
 
@@ -22,7 +25,37 @@ const ListItem = ({ content }) => (
 
 export const AboutUs = () => {
   const mdUp = useResponsive('up', 'md');
+
+  const parallaxRef = useRef(null);
+
+  const control = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
   //   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (inView) {
+      control.start('visible');
+    }
+  }, [control, inView]);
+
+  useEffect(() => {
+    const parallaxContainer = parallaxRef.current;
+
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      parallaxContainer.style.backgroundPositionY = `${scrollTop * 0.5}px`; // Adjust 0.5 for the parallax speed
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Container maxWidth="lg" sx={{ py: 8, mt: 4 }}>
@@ -33,58 +66,59 @@ export const AboutUs = () => {
           flexWrap={{ xs: 'wrap', md: 'nowrap' }}
         >
           <Grid item md={5}>
-            <Stack
-              direction="row"
-              justifyContent={mdUp ? 'flex-start' : 'center'}
-              alignItems="center"
-              gap={2}
-            >
-              <SquareBox />
-              <Typography fontWeight={700}>ABOUT US</Typography>
-            </Stack>
-
-            <Typography variant="h3" mt={2} textAlign={mdUp ? 'left' : 'center'}>
-              Your Trusted HVAC Experts
-            </Typography>
-
-            <Divider sx={{ my: 3 }}>
-              <Iconify icon="fa-solid:fan" width={28} color="#7AC142" />
-            </Divider>
-
-            <Typography variant="body1">
-              At HVAC Negotiators, we are about providing you with a seamless experience from start
-              to finish. With our three-step process, you are not just a customer; you are a valued
-              partner. HVAC Negotiators will transform your HVAC journey into a hassle-free and
-              rewarding adventure.
-            </Typography>
-
-            <Stack direction="column" alignItems="flex-start" gap={1.8} mt={4}>
-              <ListItem content="Customized HVAC solutions to match unique needs." />
-              <ListItem content="Focus on durability, energy efficiency, and reliability." />
-              <ListItem content="Certified and experienced HVAC technicians." />
-              <ListItem content="Promoting eco-friendly HVAC options." />
-            </Stack>
-
-            {/* <Button
-              sx={{
-                minHeight: 40,
-                bgcolor: '#7AC142',
-                color: '#ffffff',
-                borderRadius: 0,
-                ':hover': {
-                  bgcolor: '#022a5c',
-                },
-                width: mdUp ? '50%' : '100%',
-                mt: 4,
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0, x: -20 }}
+              animate={control}
+              variants={{
+                visible: { opacity: 1, x: 0, transition: { duration: 0.3 } },
               }}
-              onClick={() => navigate('/auth')}
             >
-              <Typography fontSize={14}>More About Us</Typography>
-            </Button> */}
+              <Stack
+                direction="row"
+                justifyContent={mdUp ? 'flex-start' : 'center'}
+                alignItems="center"
+                gap={2}
+              >
+                <SquareBox />
+                <Typography fontWeight={700}>ABOUT US</Typography>
+              </Stack>
+
+              <Typography variant="h3" mt={2} textAlign={mdUp ? 'left' : 'center'}>
+                Your Trusted HVAC Experts
+              </Typography>
+
+              <Divider sx={{ my: 3 }}>
+                <Iconify icon="fa-solid:fan" width={28} color="#7AC142" />
+              </Divider>
+
+              <Typography variant="body1">
+                At HVAC Negotiators, we are about providing you with a seamless experience from
+                start to finish. With our three-step process, you are not just a customer; you are a
+                valued partner. HVAC Negotiators will transform your HVAC journey into a hassle-free
+                and rewarding adventure.
+              </Typography>
+
+              <Stack direction="column" alignItems="flex-start" gap={1.8} mt={4}>
+                <ListItem content="Customized HVAC solutions to match unique needs." />
+                <ListItem content="Focus on durability, energy efficiency, and reliability." />
+                <ListItem content="Certified and experienced HVAC technicians." />
+                <ListItem content="Promoting eco-friendly HVAC options." />
+              </Stack>
+            </motion.div>
           </Grid>
 
           <Grid item md={5}>
-            <img src={AboutUsImg} alt="about-us" width="100%" />
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0, x: 20 }}
+              animate={control}
+              variants={{
+                visible: { opacity: 1, x: 0, transition: { duration: 0.3 } },
+              }}
+            >
+              <img src={AboutUsImg} alt="about-us" width="100%" />
+            </motion.div>
           </Grid>
         </Grid>
       </Container>
@@ -126,10 +160,6 @@ export const AboutUs = () => {
               </Typography>
             </Stack>
           </Stack>
-
-          {/* <Button sx={{ fontWeight: 400, borderRadius: 0 }} variant="contained">
-            REQUST ESTIMATES
-          </Button> */}
         </Stack>
       </Stack>
     </>
--- src/layouts/home/our-process.jsx
+++ src/layouts/home/our-process.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prop-types */
-import React from 'react';
+
 import PropTypes from 'prop-types';
+import React, { useRef, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import { Box, Card, Stack, Rating, Divider, Container, Typography } from '@mui/material';
+
+import AboutUsBgImg from 'src/assets/images/about-us-bg.jpg';
 
 import Iconify from 'src/components/iconify';
 
@@ -29,99 +35,159 @@ const processCardData = [
 
 const SquareBox = () => <Box width={16} height={16} bgcolor="#7AC142" />;
 
-const ProcessCard = ({ icon, title, description, step = 1 }) => (
-  <Card
-    sx={{
-      maxWidth: 340,
-      minHeight: 360,
-      maxHeight: 360,
-      borderRadius: 0,
-      bgcolor: '#F9F6EF',
-      p: 2,
-    }}
-  >
-    <Stack
-      position="absolute"
-      justifyContent="center"
-      alignItems="center"
-      width={124}
-      height={20}
-      bgcolor="#04192a"
-      top={24}
-      left={-28}
-      sx={{ transform: 'rotate(-45deg)' }}
+const ProcessCard = ({ icon, title, description, step = 1 }) => {
+  const control = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: 0.4,
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      control.start('visible');
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, x: 20 }}
+      animate={control}
+      variants={{
+        visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+      }}
     >
-      <Typography color="#FFFFFF" variant="subtitle2">
-        STEP {step}
-      </Typography>
-    </Stack>
-
-    <Stack width="100%" minHeight={168} direction="row" alignItems="center" justifyContent="center">
-      <Iconify icon={icon} width={64} color="#7AC142" />
-    </Stack>
-
-    <Stack gap={1} direction="column" alignItems="center" justifufyContent="center">
-      <Typography textAlign="center" gutterBottom variant="h4" component="div">
-        {title}
-      </Typography>
-      <Typography textAlign="center" variant="body2">
-        {description}
-      </Typography>
-    </Stack>
-  </Card>
-);
-
-export const OurProcess = () => (
-  <Box mt={4}>
-    <Container maxWidth="lg" sx={{ py: 8 }}>
-      <Stack direction="row" justifyContent="center" alignItems="center" gap={2}>
-        <SquareBox />
-        <Typography fontWeight={700}>OUR PROCESS</Typography>
-      </Stack>
-
-      <Typography variant="h3" mt={2} textAlign="center">
-        How It Works?
-      </Typography>
-
-      <Divider sx={{ my: 3 }}>
-        <Iconify icon="fa-solid:tools" width={28} color="#7AC142" />
-      </Divider>
-
-      <Typography variant="subtitle1" textAlign="center">
-        Here are three steps outlining how HVAC Negotiators system works:
-      </Typography>
-
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="flex-start"
-        flexWrap="wrap"
-        gap={4}
-        mt={4}
+      <Card
+        sx={{
+          maxWidth: 340,
+          minHeight: 360,
+          maxHeight: 360,
+          borderRadius: 0,
+          bgcolor: '#F9F6EF',
+          p: 2,
+        }}
       >
-        {processCardData.map((item, index) => (
-          <ProcessCard
-            key={item.title}
-            icon={item.icon}
-            title={item.title}
-            description={item.description}
-            step={index + 1}
-          />
-        ))}
-      </Stack>
+        <Stack
+          position="absolute"
+          justifyContent="center"
+          alignItems="center"
+          width={124}
+          height={20}
+          bgcolor="#04192a"
+          top={24}
+          left={-28}
+          sx={{ transform: 'rotate(-45deg)' }}
+        >
+          <Typography color="#FFFFFF" variant="subtitle2">
+            STEP {step}
+          </Typography>
+        </Stack>
 
-      <Typography variant="body1" textAlign="center" mt={4}>
-        &quot;At HVAC Negotiators, we deliver a 5-star experience:&quot;
-      </Typography>
+        <Stack
+          width="100%"
+          minHeight={168}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Iconify icon={icon} width={64} color="#7AC142" />
+        </Stack>
 
-      <Stack direction="row" justifyContent="center" alignItems="center" mt={2}>
-        <Rating value={5} readOnly sx={{ gap: 2 }} size="large" />
-      </Stack>
-    </Container>
+        <Stack gap={1} direction="column" alignItems="center" justifufyContent="center">
+          <Typography textAlign="center" gutterBottom variant="h4" component="div">
+            {title}
+          </Typography>
+          <Typography textAlign="center" variant="body2">
+            {description}
+          </Typography>
+        </Stack>
+      </Card>
+    </motion.div>
+  );
+};
 
-    <Box height={10} />
-  </Box>
-);
+export const OurProcess = () => {
+  const parallaxRef = useRef(null);
+
+  useEffect(() => {
+    const parallaxContainer = parallaxRef.current;
+
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      parallaxContainer.style.backgroundPositionY = `${scrollTop * 0.5}px`; // Adjust 0.5 for the parallax speed
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <Box>
+      <Box
+        ref={parallaxRef}
+        sx={{
+          backgroundImage: `url(${AboutUsBgImg})`,
+          backgroundAttachment: 'fixed',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          my: 12,
+        }}
+      >
+        <Box bgcolor="rgba(249, 246, 239, 0.4)">
+          <Container maxWidth="lg" sx={{ py: 8 }}>
+            <Stack direction="row" justifyContent="center" alignItems="center" gap={2}>
+              <SquareBox />
+              <Typography fontWeight={700} color="#04192a">
+                OUR PROCESS
+              </Typography>
+            </Stack>
+
+            <Typography variant="h3" mt={2} textAlign="center" color="#04192a">
+              How It Works?
+            </Typography>
+
+            <Divider sx={{ my: 3 }}>
+              <Iconify icon="fa-solid:tools" width={28} color="#7AC142" />
+            </Divider>
+
+            <Typography variant="subtitle1" textAlign="center" color="#04192a">
+              Here are three steps outlining how HVAC Negotiators system works:
+            </Typography>
+
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="flex-start"
+              flexWrap="wrap"
+              gap={4}
+              mt={4}
+            >
+              {processCardData.map((item, index) => (
+                <ProcessCard
+                  key={item.title}
+                  icon={item.icon}
+                  title={item.title}
+                  description={item.description}
+                  step={index + 1}
+                />
+              ))}
+            </Stack>
+
+            <Typography variant="body1" textAlign="center" mt={4} color="#04192a">
+              &quot;At HVAC Negotiators, we deliver a 5-star experience:&quot;
+            </Typography>
+
+            <Stack direction="row" justifyContent="center" alignItems="center" mt={2}>
+              <Rating value={5} readOnly sx={{ gap: 2 }} size="large" />
+            </Stack>
+          </Container>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 ProcessCard.propTypes = {
   step: PropTypes.number,
